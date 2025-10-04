8c2b35deabf02fa9adfc80a32202dfdda1fc694c
Manoj <manojvsmaddula@gmail.com>
2025-10-04 23:19:02 +0530

    Update procedure_bronze.sql
    Removed the paths.

--- scripts/bronze/procedure_bronze.sql
+++ scripts/bronze/procedure_bronze.sql
@@ -40,7 +40,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.crm_cust_info'
 		BULK INSERT bronze.crm_cust_info
-		FROM 'D:\Project Odin\Data-Warehouse\datasets\source_crm\cust_info.csv'
+		FROM '...\cust_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -59,7 +59,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.crm_prd_info'
 		BULK INSERT bronze.crm_prd_info
-		FROM 'D:\Project Odin\Data-Warehouse\datasets\source_crm\prd_info.csv'
+		FROM '...\prd_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -78,7 +78,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.crm_sales_details'
 		BULK INSERT bronze.crm_sales_details
-		FROM 'D:\Project Odin\Data-Warehouse\datasets\source_crm\sales_details.csv'
+		FROM '...\sales_details.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -101,7 +101,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.erp_cust_az12'
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'D:\Project Odin\Data-Warehouse\datasets\source_erp\cust_az12.csv'
+		FROM '...\cust_az12.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -120,7 +120,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.erp_loc_a101'
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'D:\Project Odin\Data-Warehouse\datasets\source_erp\loc_a101.csv'
+		FROM '...\loc_a101.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -139,7 +139,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2'
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'D:\Project Odin\Data-Warehouse\datasets\source_erp\px_cat_g1v2.csv'
+		FROM '...\px_cat_g1v2.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
